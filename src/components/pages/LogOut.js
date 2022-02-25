4c84a12dba3acdecb44d9b132bfde3bb9aa11843
Karol855 <karl.wierczuk@gmail.com>
2022-02-25 12:25:44 +0100

    Login, Logout, Register

--- src/components/pages/LogOut.js
+++ src/components/pages/LogOut.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import decoration from "../../assets/Decoration.svg";
 
 const LogOut = () => {
     return (
-        <div>
-            wylogowanie
-        </div>
+        <section className="logout">
+            <div className="title">
+                <h2 className="title__text">Wylogowano się pomyślnie!</h2>
+                <img className="title__img" src={decoration} alt="Decoration" />
+            </div>
+            <Link to="/" className="button">
+                Strona główna
+            </Link>
+        </section>
     );
 };
 
